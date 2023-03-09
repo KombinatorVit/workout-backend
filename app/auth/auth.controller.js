3953131774293cfee1a19352a72653279ee3cfc5
KombinatorVit <slonik0775@gmail.com>
2023-03-09 21:58:32 +0200

    add prisma && user Model

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -2,6 +2,9 @@
 // @route   POST /api/auth/login
 // @access  Public
 
+import { prisma } from "../prisma.js";
+
 export const authUser = async (reg, res) => {
-  res.json({ message: "You are authenticated" });
+  const user = await prisma.user.findMany();
+  res.json(user);
 };
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import authRoutes from "./app/auth/auth.routes.js";
 import "colors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import { prisma } from "./app/prisma.js";
 
+dotenv.config();
 const app = express();
 
 async function main() {
-  // if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+  if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
   app.use(express.json());
   app.use("/api/auth", authRoutes);
@@ -21,4 +25,12 @@ async function main() {
   );
 }
 
-main();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
