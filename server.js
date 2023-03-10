df294dae47c53968632ea75ee32989e1bc700d1d
KombinatorVit <slonik0775@gmail.com>
2023-03-10 02:11:38 +0200

    workout routers and controller

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import { errorHandler, notFound } from "./app/middleware/error.middleware.js";
 import userRoutes from "./app/user/user.routes.js";
 import path from "path";
 import exerciseRoutes from "./app/exercise/exercise.routes.js";
+import workoutRoutes from "./app/workout/workout.routes.js";
 
 dotenv.config();
 const app = express();
@@ -23,6 +24,7 @@ async function main() {
   app.use("/api/auth", authRoutes);
   app.use("/api/users", userRoutes);
   app.use("/api/exercises", exerciseRoutes);
+  app.use("/api/workouts", workoutRoutes);
 
   app.use(notFound);
   app.use(errorHandler);
